ecd120b8d07f629e8b117335546ae9cc8ec8a5de
rodrigo.silveira <rodrigo.silveira@zanthus.com.br>
2021-02-12 16:37:30 -0300

    subindo arquivos

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Page;
+use App\Models\Link;
+
+class PageController extends Controller
+{
+    public function index($slug) {
+        $page = Page::where('slug', $slug)->first();
+        if($page) {
+            // Background color or picture
+            $bg = '#FFFFFF';
+            switch($page->op_bg_type) {
+                case 'image':
+                    $bg = "url('".url('/media/uploads')."/".$page->op_bg_value."')";
+                break;
+                case 'color':
+                    $colors = explode (',', $page->op_bg_value);
+                    $bg_temp = 'linear-gradient(90deg,';
+                    $bg_temp .= $colors[0].',';
+                    $bg_temp .= $colors[1] ?? $bg;
+                    $bg_temp .= ')';
+                    $bg = $bg_temp;
+                break;
+            }
+
+
+
+            // Links
+            $links = Link::where('id_page', $page->id)
+                ->where('status', 1)
+                ->orderBy('order')
+                ->get();
+
+
+            // Registrar a visualização
+
+            return view('page', [
+                'font_color'=> $page->op_font_color,
+                'profile_image' => url('media/uploads').'/'.$page->op_profile_image,
+                'title' => $page->op_title,
+                'description' => $page->op_description,
+                'fb_pixel' => $page->op_fb_pixel,
+                'bg' => $bg,
+                'links' => $links
+
+            ]);
+        } else {
+            return view('notfound');
+        }
+    }
+}
